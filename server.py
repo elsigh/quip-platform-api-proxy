866e360b22239ae1fa0484d60cbb904a0b382a15
Lindsey Simon <lindsey@quip.com>
2018-07-20 14:15:36 -0700

    update again

--- server.py
+++ server.py
@@ -7,25 +7,24 @@ PORT_NUMBER = 8080
 #the browser
 class myHandler(BaseHTTPRequestHandler):
 
-	#Handler for the GET requests
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header('Content-type','text/html')
-		self.end_headers()
-		# Send the html message
-		self.wfile.write("Hello World !")
-		return
+    #Handler for the GET requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        self.wfile.write("Hello World !")
+        return
 
 try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-    print('Started httpserver on port ' , PORT_NUMBER)
-
-	#Wait forever for incoming htto requests
-	server.serve_forever()
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print('Started httpserver on port %s', PORT_NUMBER)
+    # Wait forever for incoming htto requests
+    server.serve_forever()
 
 except KeyboardInterrupt:
-	print('^C received, shutting down the web server')
+    print('^C received, shutting down the web server')
 
-	server.socket.close()
+    server.socket.close()
