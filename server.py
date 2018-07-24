157670cb527303e48f95fa5a59d7fa6d922ed48b
Lindsey Simon <lindsey@quip.com>
2018-07-24 08:04:45 -0700

    updates

--- server.py
+++ server.py
@@ -24,12 +24,12 @@ class new:
         web.header('Access-Control-Allow-Credentials', 'true')
         web.header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
 
-    def create_new_from_template(self, access_token, template_thread_id, member_ids):
+    def create_new_from_template(self, access_token, template_thread_id, title, member_ids):
         client = quip.QuipClient(access_token)
         template_json = client.get_thread(template_thread_id)
         template_html = template_json.get("html")
 
-        new_html = template_html.replace("Feedback For Engineers Template", "Feedback For Lindsey From Kurchi")
+        new_html = template_html.replace("Feedback For Engineers Template", title)
         logging.debug("new_html %s", new_html)
         new_thread_response = client.new_document(new_html, member_ids=member_ids)
         logging.debug("new_thread_response %s", new_thread_response)
@@ -49,16 +49,19 @@ class new:
 
     def POST(self):
         self.cors_headers()
+
         web_data = json.loads(web.data())
         logging.debug("WEB DATA %s", web_data)
-
         access_token = web_data.get('access_token')
         template_thread_id = web_data.get('template_thread_id')
+        member_ids = web_data.get('member_ids')
+        title = web_data.get('title')
 
-        new_document_response = self.create_new_from_template(access_token, template_thread_id)
+        new_document_response = self.create_new_from_template(access_token,
+            template_thread_id, title, member_ids)
 
         web.header('Content-Type', 'application/json')
-        return json.dumps({'message': "POST OK!", 'api_response': new_document_response})
+        return json.dumps({'api_response': new_document_response})
 
 if __name__ == "__main__":
     print('running')
